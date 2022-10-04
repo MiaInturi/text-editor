095f4c158193136311e8df48bad833da0d5a5bae
Nursultan Zianurov <zianurovnursultan1@gmail.com>
2022-10-05 00:31:07 +0700

    feature/text-editor/3 update jest config and define packages versions

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig')
+
 const jestConfig = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' })
 }
 
 module.exports = jestConfig
